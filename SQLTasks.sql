9559a76d1801e4bdb537eef22dad43bf147e8f7a
Kevin Kwasnik <kfkwasnik@gmail.com>
2022-07-25 15:17:51 -0400

    recent signup no limit clause

--- SQLTasks.sql
+++ SQLTasks.sql
@@ -47,21 +47,35 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
-
+SELECT facid, name, membercost, monthlymaintenance
+FROM Facilities
+WHERE membercost > 0 AND membercost < (.2 * monthlymaintenance);
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
 
+SELECT *
+FROM Facilities
+WHERE facid IN (1,5);
 
 /* Q5: Produce a list of facilities, with each labelled as
 'cheap' or 'expensive', depending on if their monthly maintenance cost is
 more than $100. Return the name and monthly maintenance of the facilities
 in question. */
 
+SELECT name, monthlymaintenance,
+		CASE WHEN monthlymaintenance < 100 THEN 'cheap'
+			   WHEN monthlymaintenance > 100 THEN 'expensive'
+			   END AS expense
+FROM Facilities;
 
 /* Q6: You'd like to get the first and last name of the last member(s)
 who signed up. Try not to use the LIMIT clause for your solution. */
 
+SELECT firstname, surname
+FROM Members
+WHERE joindate = (SELECT MAX(joindate)
+				            FROM Members);
 
 /* Q7: Produce a list of all members who have used a tennis court.
 Include in your output the name of the court, and the name of the member
